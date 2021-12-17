b159f038730168b7a13e160cd63de752db7987cf
Gursimran Singh <gurisingh262001@gmail.com>
2021-12-17 21:10:18 +0530

    "Title changed" and "Head component added"

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+import Head from "next/head";
+
 export default function FirstPost() {
-  return <h1>FIRST POST</h1>;
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>FIRST POST</h1>
+      go back to home <br />
+      <Link href={"/"}>
+        <a> {"<==="}</a>
+      </Link>
+    </>
+  );
 }
